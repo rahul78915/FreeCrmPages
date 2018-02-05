018529ce9a39b160d47724cd299097407fa31a3c
rahul78915 <rahulprakash0862@gmail.com>
2018-02-05 22:08:46 +0530

    adding crm project

---
+++ src/test/java/com/crm/qa/tests/LoginPageTest.java
@@ -0,0 +1,53 @@
+package com.crm.qa.tests;
+
+
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase {
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginPageTest(){
+		
+		super();
+	}
+	
+
+	
+	
+	@BeforeMethod()
+	public void setUp(){
+		initalization();
+		 loginPage= new LoginPage();
+		 
+	}
+	@Test(priority=1)
+	public void loginPageTitleTest(){
+		String title=loginPage.ValidateLoginPagetitle();
+	Assert.assertEquals(title, "#1 Free CRM for Any Business: Online Customer Relationship Software");
+	}
+	@Test(priority=2)
+	public void CRMImageTest(){
+		
+	boolean Flag=loginPage.validateCRMImage();
+		Assert.assertTrue(Flag);
+	}
+	@Test(priority=3)
+	public void loginTest(){
+		homePage=loginPage.login(prop.getProperty("userename"),prop.getProperty("password"));
+		
+	}
+	
+	@AfterMethod()
+	public void tearDown(){
+		driver.quit();
+	}
+}
